00582ff25b411009d5da018b04b5b10c78eee539
João Victor <joaovictorpinheiro2007@gmail.com>
2025-12-02 13:07:21 -0300

    nvim: fix tabs and spellcheck

---
+++ src/.config/nvim/lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.g.lazyvim_prettier_needs_config = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smarttab = true
+
+vim.opt.spell = true
+vim.opt.spelllang = { "en", "pt_BR" }
